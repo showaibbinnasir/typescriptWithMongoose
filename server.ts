3cfd2c27c478e94ceedea669c8eb8dd08cc01cf1
showaibbinnasir <showaibnishad@gmail.com>
2023-08-18 00:56:04 -0500

    first com

---
+++ server.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+import app from './app';
+
+
+const port: number = 5000
+
+
+//database connection
+
+async function starter() {
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/firstmongoose');
+        console.log(`Database connected`);
+        app.listen(port, () => {
+            console.log(`server is listening on port ${port}`)
+          })
+    }catch{
+        console.log(`failed to connect database`);
+    }
+}
+
+starter();
+
+
+
